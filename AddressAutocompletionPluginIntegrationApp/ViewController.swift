8a0cfbd1a3e4540cc99022bf7d3dbae4bbc6936d
Ivan Yanakiev <ivan.yanakiev@sonect.ch>
2020-04-09 10:33:02 +0300

    Removed API.

--- AddressAutocompletionPluginIntegrationApp/ViewController.swift
+++ AddressAutocompletionPluginIntegrationApp/ViewController.swift
@@ -12,10 +12,17 @@ import SonectShop
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var apiKeyTextField: UITextField!
+    @IBOutlet weak var runPluginButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let plugin = SNCGoogleAddressAutocompletionPlugin(apiKey: "AIzaSyDxPjg7nmdWmUs2L9DpF2nk33u4ds8gEKw")
+        
+    }
+    
+    @IBAction func runPluginTapped(_ sender: Any) {
+        let plugin = SNCGoogleAddressAutocompletionPlugin(apiKey: apiKeyTextField.text!)
         
         plugin.addressAutocompletion(forSearchTerm: "Dobri Voynikov", countryCode: "BG") { (addressAutocompletion, error) in
             print(String(describing: addressAutocompletion))
