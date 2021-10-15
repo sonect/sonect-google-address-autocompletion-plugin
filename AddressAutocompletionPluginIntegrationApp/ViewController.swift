a012af5e2fb2003ebe6496be43e7a0098c4fdc95
antonyermilin <anton.yermilin@sonect.ch>
2021-10-15 15:20:14 +0300

    uncommented sample app code

--- AddressAutocompletionPluginIntegrationApp/ViewController.swift
+++ AddressAutocompletionPluginIntegrationApp/ViewController.swift
@@ -35,22 +35,22 @@ class ViewController: UIViewController {
         
         let searchTerm = searchTextField?.text ?? "pizza"
         
-//        plugin.addressAutocompletion(forSearchTerm: searchTerm, countryCode: countryCode) { (addressAutocompletion, error) in
-//            print(String(describing: addressAutocompletion))
-//        }
-//
-//        plugin.adressDetails(forAddressId: "EhxMaXZlcnBvb2wgU3RyZWV0LCBMb25kb24sIFVLIi4qLAoUChIJqeLSXbIcdkgRqAbi6bi_PEcSFAoSCfPzF7dbG3ZIEQqyADl5LpFJ") { (details, error) in
-//            print(String(describing: details))
-//        }
-//
-//        plugin.shops(forSearchTerm: searchTerm, countryCode: countryCode) { (shopSearch, error) in
-//            print(String(describing: shopSearch))
-//        }
-//
-//        plugin.shopDetails(forShopId: "ChIJBZ0aNKes2EcRou8EZfpOOy0") { (details, error) in
-//            print(String(describing: details))
-//            self.imageView.image = details?.shopImage
-//        }
+        plugin.addressAutocompletion(forSearchTerm: searchTerm, countryCode: countryCode) { (addressAutocompletion, error) in
+            print(String(describing: addressAutocompletion))
+        }
+
+        plugin.adressDetails(forAddressId: "EhxMaXZlcnBvb2wgU3RyZWV0LCBMb25kb24sIFVLIi4qLAoUChIJqeLSXbIcdkgRqAbi6bi_PEcSFAoSCfPzF7dbG3ZIEQqyADl5LpFJ") { (details, error) in
+            print(String(describing: details))
+        }
+
+        plugin.shops(forSearchTerm: searchTerm, countryCode: countryCode) { (shopSearch, error) in
+            print(String(describing: shopSearch))
+        }
+
+        plugin.shopDetails(forShopId: "ChIJBZ0aNKes2EcRou8EZfpOOy0") { (details, error) in
+            print(String(describing: details))
+            self.imageView.image = details?.shopImage
+        }
         
         removePlaceViews()
         
